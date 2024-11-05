50bc041f68d7cfdf47b5f7473b28beb3aafea386
Deepak <deepakprakashpillai@gmail.com>
2024-11-05 12:23:25 +0530

    Conflicts handled 2

--- Backend/diary/models.py
+++ Backend/diary/models.py
@@ -1,7 +1,7 @@
 from pyexpat import model
 from django.db import models
 from django.contrib.auth import get_user_model
-from multiselectfield import MultiSelectField  # Make sure to install this package
+from multiselectfield import MultiSelectField  
 
 User = get_user_model()
 
